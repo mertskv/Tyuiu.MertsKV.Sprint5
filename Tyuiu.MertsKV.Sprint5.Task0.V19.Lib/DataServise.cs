8f38a826db69b9560f65e5c96800f903071ed954
Кристина Мерц <Tyuiu.mertskv@outlook.com>
2025-11-19 20:42:45 +0500

    Add project files.

---
+++ Tyuiu.MertsKV.Sprint5.Task0.V19.Lib/DataServise.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.MertsKV.Sprint5.Task0.V19.Lib
+{
+    public class DataServise : ISprint5Task0V19
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            double res = (2 * Math.Pow(x, 2) - 1) / Math.Sqrt(Math.Pow(x, 2) - 2);
+            res = Math.Round(res, 3);
+            File.WriteAllText(path, Convert.ToString(res));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MertsKV.Sprint5.Task0.V19.Test/DataServiseTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.MertsKV.Sprint5.Task0.V19.Lib;
+using System.IO;
+namespace Tyuiu.MertsKV.Sprint5.Task0.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\kmert\source\repos\Tyuiu.MertsKV.Sprint5\Tyuiu.MertsKV.Sprint5.Task0.V19\bin\Debug\net8.0";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists; //Проверка сущ-ет ли файл
+            bool wait = false;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
