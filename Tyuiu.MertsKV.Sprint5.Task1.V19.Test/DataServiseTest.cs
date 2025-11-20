e7cc0af4ff6ddb0f53ffb9be83036767424e4acd
Кристина Мерц <Tyuiu.mertskv@outlook.com>
2025-11-20 20:46:11 +0500

    Task1

---
+++ Tyuiu.MertsKV.Sprint5.Task1.V19.Test/DataServiseTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.MertsKV.Sprint5.Task1.V19.Lib;
+using System.IO;
+namespace Tyuiu.MertsKV.Sprint5.Task1.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiseTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\kmert\source\repos\Tyuiu.MertsKV.Sprint5\Tyuiu.MertsKV.Sprint5.Task1.V19\bin\Debug\OutPutFileTask1.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
